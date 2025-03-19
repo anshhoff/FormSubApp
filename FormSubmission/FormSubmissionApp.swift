abfcff7d59babed195903cb8c1d150b3298f9d9b
Anshhoff <anshh2488@gmail.com>
2025-03-19 11:48:04 +0530

    Initial Commit

---
+++ FormSubmission/FormSubmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormSubmissionApp.swift
+//  FormSubmission
+//
+//  Created by Ansh Hardaha on 19/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FormSubmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FormSubmissionTests/FormSubmissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  FormSubmissionTests.swift
+//  FormSubmissionTests
+//
+//  Created by Ansh Hardaha on 19/03/25.
+//
+
+import Testing
+@testable import FormSubmission
+
+struct FormSubmissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
